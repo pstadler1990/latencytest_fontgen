8a1b92b6ab5ffa3f1514fa096952dddc6d233d72
pstadler1990 <pstadler90@gmx.de>
2019-10-03 23:40:58 +0200

    Initial commit

---
+++ fontgen.py
@@ -0,0 +1,23 @@
+"""
+Simple script to automatically convert all printable ASCII characters
+to bitmap font arrays via imagemagick
+"""
+import os
+
+FONT = 'FreeMono'
+SIZE = '16'
+OUTPUT_DIR = 'output/'
+
+# Printable (visible) ascii range is 33..126
+# http://facweb.cs.depaul.edu/sjost/it212/documents/ascii-pr.htm
+for c in range(33, 100):
+    if c is 92:
+        # skip \
+        continue
+    if c in [34, 39, 62, 96]:
+        # Escape those chars
+        use_esc = str("\\")
+    else:
+        use_esc = ''
+    os.system('convert -font {font} -pointsize {size} label:"{esc}{char}" {dir}{num}.xbm'
+              .format(font=FONT, size=SIZE, char=chr(c), esc=use_esc, num=c, dir=OUTPUT_DIR))
