e0ebe30aefd2675b60b5b8b5dd9a4c9c2102c0aa
pstadler1990 <pstadler90@gmx.de>
2019-10-04 00:59:11 +0200

    Initial commit

--- fontgen.py
+++ fontgen.py
@@ -10,7 +10,7 @@ OUTPUT_DIR = 'output/'
 
 # Printable (visible) ascii range is 33..126
 # http://facweb.cs.depaul.edu/sjost/it212/documents/ascii-pr.htm
-for c in range(33, 126):
+for c in range(33, 100):
     if c is 92:
         # skip \
         continue
@@ -19,5 +19,6 @@ for c in range(33, 126):
         use_esc = str("\\")
     else:
         use_esc = ''
-    os.system('convert -font {font} -pointsize {size} label:"{esc}{char}" {dir}{num}.xbm'
+    os.system('convert -font {font} -pointsize {size} label:"{esc}{char}" {dir}tmp.xbm'
               .format(font=FONT, size=SIZE, char=chr(c), esc=use_esc, num=c, dir=OUTPUT_DIR))
+
