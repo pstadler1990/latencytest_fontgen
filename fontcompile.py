f56a771734d0b1c73f07d0e56f2af0bf2597410f
pstadler1990 <pstadler90@gmx.de>
2019-10-04 16:07:45 +0200

    Manually fixed some characters in the font

--- fontcompile.py
+++ fontcompile.py
@@ -6,21 +6,37 @@ import io
 import re
 import glob
 
-INPUT_DIR = 'output/'
-OUTPUT_FILE = 'font.h'
-
-RE_MATCH = re.compile(r'(\d+)_bits')
+INPUT_DIR = 'font_monospaced_16px/'
+OUTPUT_FILE = 'font_monospaced16px.h'
 
 os.chdir(INPUT_DIR)
-out_str = '// Font file generated with fontcompile.py\n'
-for file in glob.glob("*.xbm"):
-    with io.open(file, 'r') as xbm_file:
-        l = xbm_file.readlines()
-        if len(l) is not 6:
-            print("ERROR", file)
-        # Merge lines 2..6 and prefix the variable with "c_" to be valid in the C language
-        str_merged = re.sub(RE_MATCH, r'c_\1', ''.join(l[2:6]))
-        out_str += str_merged
+files = 0
+out_str = ''
+for n in range(33, 126):
+    try:
+        with io.open('{}.xbm'.format(n), 'r') as xbm_file:
+            ln = xbm_file.readlines()
+            # Merge lines 3..6 and prefix the variable with "c_" to be valid in the C language
+            str_merged = r''.join(ln[3:6]).replace('\n', '').replace('   ', '').replace(' };', '')
+            out_str += '{' + str_merged + '},\n'
+            files += 1
+    except FileNotFoundError:
+        print("File not found", n)
+        out_str += '{' + '0x00, ' * 34 + '},\n'
+        files += 1
+
+begin_str = '''// Font file generated with fontcompile.py
+#define FONT_WIDTH ((uint32_t)11)
+#define FONT_HEIGHT ((uint32_t)17)
+#define FONT_CHAR_START ((uint32_t)33)
+#define FONT_CHAR_END ((uint32_t)126)
+
+static unsigned char chars[''' + str(files) + '''][''' + str(34) + '''] = {
+'''
+
+end_str = '\n};'
 
 with io.open(OUTPUT_FILE, 'w') as out_file:
+    out_file.write(begin_str)
     out_file.write(out_str)
+    out_file.write(end_str)
